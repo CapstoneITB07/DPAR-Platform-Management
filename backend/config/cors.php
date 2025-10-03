53b4e54addc587a4c2091d17a7bc45e95c6349b3
Carlo <markcarlo0926@gmail.com>
2025-10-03 19:12:21 +0800

    After Deployment files

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'storage/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://dparvc.com' // Fallback for production
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'],
